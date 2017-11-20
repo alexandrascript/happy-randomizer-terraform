d5aa243f605265220e1bd189fc7bd89a29a7cc90
Casey Bisson <casey.bisson@joyent.com>
2017-11-20 08:15:55 -0800

    re-order blue and green, add comments

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+#
+# Details about all deployments of this application
+#
 variable "production" {
   type = "string"
   default = "blue"
@@ -12,28 +15,34 @@ variable "service_name" {
 	default = "happiness"
 }
 
-variable "green_image_name" {
-    default = "happy_randomizer"
-}
-
-variable "green_image_version" {
-    default = "1.0.0"
-}
-
-variable "green_count" {
-    description = "Number of green machines to create"
-    default = "0"
-}
-
+#
+# Details about the "blue" deployment
+#
 variable "blue_image_name" {
-    default = "happy_randomizer"
+    default = "ubuntu-16.04"
 }
 
 variable "blue_image_version" {
-    default = "1.1.0"
+    default = "20170403"
 }
 
 variable "blue_count" {
     description = "Number of blue machines to create"
     default = "3"
+}
+
+#
+# Details about the "green" deployment
+#
+variable "green_image_name" {
+    default = "ubuntu-14.04"
+}
+
+variable "green_image_version" {
+    default = "20170403"
+}
+
+variable "green_count" {
+    description = "Number of green machines to create"
+    default = "0"
 }
